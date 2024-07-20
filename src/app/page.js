d840c0b266a66b5e3b8564503f9656fcee4aa336
ab3nxy <abenxy0@gmail.com>
2024-07-20 17:17:59 +0300

    updated for mobile view

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,3 @@
-// pages/HomePage.js
-
 "use client";
 import Image from "next/image";
 import Head from "next/head";
@@ -11,34 +9,34 @@ import Footer from "./Footer";
 
 export default function HomePage() {
   return (
-    <div className="font-sans">
+    <div className="font-sans min-h-screen flex flex-col">
       <Head>
         <title>AbenXY</title>
         <meta name="description" content="Portfolio of Aben, Full Stack Developer and Machine Learning Enthusiast" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
-      <main className="bg-white pt-2 px-10 dark:bg-gray-900 md:px-20 lg:px-40 font-sans">
-        <div className="text-center p-8 py-2" id="home" >
-            <h2 className="text-5xl mt-20 py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-              Abenezer Shiferaw
-            </h2>
-            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-              Full Stack Developer
-            </h3>
-            <div className="mx-auto rounded-full w-80 h-80 relative overflow-hidden mt-10 md:h-96 md:w-96">
-              <Image
-                src="/profile.png"
-                layout="fill"
-                objectFit="contain"
-                alt="Developer"
-              />
-            </div>
-            <p className="text-xl text-gray-700 dark:text-gray-300 md:mx-20">
-              Experienced in building web applications with modern technologies.
-              I can help you bring your web app ideas to life!
-            </p>
+      <main className="flex-grow pt-2 px-4 bg-gray-900 md:px-20 lg:px-40 font-sans">
+        <div className="text-center p-8 py-2" id="home">
+          <h2 className="text-5xl mt-20 py-2 text-teal-600 font-medium md:text-6xl">
+            Abenezer Shiferaw
+          </h2>
+          <h3 className="text-2xl py-2 md:text-3xl">
+            Full Stack Developer
+          </h3>
+          <div className="mx-auto rounded-full w-64 h-64 relative overflow-hidden mt-10 md:h-96 md:w-96">
+            <Image
+              src="/profile.png"
+              layout="fill"
+              objectFit="contain"
+              alt="Developer"
+            />
           </div>
+          <p className="text-xl text-gray-300 mt-4 md:mx-20">
+            Experienced in building web applications with modern technologies.
+            I can help you bring your web app ideas to life!
+          </p>
+        </div>
         <Skills />
         <Certificates />
         <Projects />
