709631b5a8c9c624881eb497003df961ed0cfd20
ab3nxy <abenxy0@gmail.com>
2024-07-20 18:01:49 +0300

    updated for mobile view

--- src/app/page.js
+++ src/app/page.js
@@ -16,12 +16,12 @@ export default function HomePage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
-      <main className="flex-grow pt-2 px-4 bg-gray-900 md:px-20 lg:px-40 font-sans">
+      <main className="flex-grow bg-white pt-2 px-4 dark:bg-gray-900 md:px-20 lg:px-40 font-sans">
         <div className="text-center p-8 py-2" id="home">
-          <h2 className="text-5xl mt-20 py-2 text-teal-600 font-medium md:text-6xl">
+          <h2 className="text-5xl mt-20 py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
             Abenezer Shiferaw
           </h2>
-          <h3 className="text-2xl py-2 md:text-3xl">
+          <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
             Full Stack Developer
           </h3>
           <div className="mx-auto rounded-full w-64 h-64 relative overflow-hidden mt-10 md:h-96 md:w-96">
@@ -32,7 +32,7 @@ export default function HomePage() {
               alt="Developer"
             />
           </div>
-          <p className="text-xl text-gray-300 mt-4 md:mx-20">
+          <p className="text-xl text-gray-700 dark:text-gray-300 mt-4 md:mx-20">
             Experienced in building web applications with modern technologies.
             I can help you bring your web app ideas to life!
           </p>
