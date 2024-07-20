8dd68ad76c1405ff61ad56bfa354822b07009c6d
ab3nxy <abenxy0@gmail.com>
2024-07-20 19:06:24 +0300

    theme and mobile responsive

--- src/app/Certificates.js
+++ src/app/Certificates.js
@@ -1,5 +1,3 @@
-// components/Certificates.js
-
 "use client";
 
 export default function Certificates() {
@@ -62,31 +60,31 @@ export default function Certificates() {
   ];
 
   return (
-    <section id="certificates" className="py-24 bg-gray-100 dark:bg-gray-900">
-      <div className="text-center mb-12">
-        <h2 className="text-5xl font-bold text-teal-600 dark:text-teal-400 mb-6">
+    <section id="certificates" className="py-20 md:py-24 bg-gray-100 dark:bg-gray-900">
+      <div className="text-center mb-6">
+        <h2 className="text-4xl md:text-5xl font-bold text-teal-600 dark:text-teal-400 mb-6">
           Certificates
         </h2>
       </div>
-      <div className="max-w-4xl mx-auto">
+      <div className="max-w-4xl mx-auto px-4">
         {certificateData.map((cert) => (
-          <div key={cert.id} className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg mb-8">
-            <h3 className="text-2xl text-center font-semibold text-teal-500 mb-4">
+          <div key={cert.id} className="bg-white dark:bg-gray-800 p-4 md:p-6 rounded-lg shadow-lg mb-8">
+            <h3 className="text-xl md:text-2xl text-center font-semibold text-teal-500 mb-4">
               {cert.title}
             </h3>
             <div className="flex flex-col md:flex-row items-center md:items-start">
-              <div className="w-full mt-4 md:w-2/5 mb-4 md:mb-0 md:pr-6">
+              <div className="w-full md:w-2/5 mb-4 md:mb-0 md:pr-6">
                 {cert.details.map((detail, index) => (
                   <button
                     key={index}
-                    className="block w-full h-auto text-left mb-1 p-2 bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-300 text-sm rounded-md hover:bg-gray-300 dark:hover:bg-gray-600 transition duration-300"
+                    className="block w-full h-auto text-left mb-2 p-2 bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-300 text-sm rounded-md hover:bg-gray-300 dark:hover:bg-gray-600 transition duration-300"
                   >
                     {detail}
                   </button>
                 ))}
               </div>
               <div className="w-full md:w-3/5 flex justify-center md:justify-end mt-4">
-                <img src={cert.image} alt={cert.title} className="w-full h-auto rounded-lg " />
+                <img src={cert.image} alt={cert.title} className="w-full h-auto rounded-lg" />
               </div>
             </div>
             <div className="mt-6 text-center">
@@ -94,7 +92,7 @@ export default function Certificates() {
                 href={cert.link}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="bg-teal-700 p-2 text-white text-lg rounded-md hover:bg-teal-600 transition duration-300 transform hover:scale-105"
+                className="bg-teal-700 p-2 text-white text-base md:text-lg rounded-md hover:bg-teal-600 transition duration-300 transform hover:scale-105"
               >
                 View Certificate Online
               </a>
